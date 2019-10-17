3807c0fc3504c3ac2fdb2c1a19e76419c880237b
kaviyapreethi <preethikaviya97@gmail.com>
2019-10-17 16:23:42 +0530

    Computed Properties -Challenge 7

--- main.js
+++ main.js
@@ -1,38 +1,55 @@
 var app = new Vue({
   el: "#app",
   data: {
+    brand: "Vue Mastery",
     product: "Socks",
     discription: "A pair of warm, fuzzy socks",
-    image: "./assets/vmSocks-green.jpg",
+    selectedVariant: 0,
     link:
       "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
-    inStock: false,
     onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
-        variantImage: "./assets/vmSocks-green.jpg"
+        variantImage: "./assets/vmSocks-green.jpg",
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage: "./assets/vmSocks-blue.jpg"
+        variantImage: "./assets/vmSocks-blue.jpg",
+        variantQuantity: 0
       }
     ],
     sizes: ["Small", "Medium", "Large"],
     cart: 0
   },
   methods: {
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
+      console.log(index);
     },
     addToCart() {
       this.cart += 1;
     },
     removeFromCart() {
       this.cart -= 1;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    sale() {
+      return this.brand + " " + this.product + " On Sale!";
     }
   }
 });
