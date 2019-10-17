58e8a9b5c5c0173634bc265206e2a6d30cffcdb4
kaviyapreethi <preethikaviya97@gmail.com>
2019-10-17 11:13:02 +0530

    Attribute bind - Challenge 2

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    discription: "A pair of warm, fuzzy socks"
+    discription: "A pair of warm, fuzzy socks",
+    image: "./assets/vmSocks-green.jpg",
+    link:
+      "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
   }
 });
